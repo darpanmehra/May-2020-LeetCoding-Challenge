e9be6b6044b9360d7aac7219e0172ec7969f8b8b
Darpan Mehra <darpan.mehra10@gmail.com>
2020-05-02 17:45:59 +0530

    May 2, 2020 Problem- Jewels and Stones

---
+++ May 2, 2020.py
@@ -0,0 +1,21 @@
+#Problem Statement:
+#You're given strings J representing the types of stones that are jewels, and S representing the stones you have.  Each character in S is a type of stone you have.  You want to know how many of the stones you have are also jewels.
+#The letters in J are guaranteed distinct, and all characters in J and S are letters. Letters are case sensitive, so "a" is considered a different type of stone from "A".
+#Example:
+#Input: J = "aA", S = "aAAbbbb"
+#Output: 3
+
+#Using List Comprehension
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        list = [jem for jem in S if jem in J]
+        return len(list)
+
+#Using Standard Process
+class Solution:
+    def numJewelsInStones(self, J: str, S: str) -> int:
+        count = 0
+        for jem in S:
+            if jem in J:
+                count+=1
+        return count

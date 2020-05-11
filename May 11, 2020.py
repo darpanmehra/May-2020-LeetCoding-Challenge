f74c11911c2ee27e049552ae74e0139e8a55e635
Darpan Mehra <darpan.mehra10@gmail.com>
2020-05-11 22:29:40 +0530

    May 11, 2020 Problem- Flood Fill

---
+++ May 11, 2020.py
@@ -0,0 +1,23 @@
+#Flood Fill
+#An image is represented by a 2-D array of integers, each integer representing the pixel value of the image (from 0 to 65535).
+#Given a coordinate (sr, sc) representing the starting pixel (row and column) of the flood fill, and a pixel value newColor, "flood fill" the image.
+#To perform a "flood fill", consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color as the starting pixel), and so on. Replace the color of all of the aforementioned pixels with the newColor.
+#At the end, return the modified image.
+#Example:
+#Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, newColor = 2
+#Output: [[2,2,2],[2,2,0],[2,0,1]]
+#Explanation: From the center of the image (with position (sr, sc) = (1, 1)), all pixels connected by a path of the same color as the starting pixel are colored with the new color. Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, newColor: int) -> List[List[int]]:
+        def filladjacent(image, i, j, color, newColor):
+            if i < 0 or i >= len(image) or j<0 or j<=len(image[0]) image[i][j] != color:
+                return
+            image[i][j] = newColor
+            filladjacent(image, i+1, j, color, newColor)
+            filladjacent(image, i-1, j, color, newColor)
+            filladjacent(image, i, j+1, color, newColor)
+            filladjacent(image, i, j-1, color, newColor)
+        if image[sr][sc] == newColor: return image
+        filladjacent(image, sr, sc, image[sr][sc], newColor)
+        return image

30b11ceee37b39dafe67977559c915cbfcedba40
Darpan Mehra <darpan.mehra10@gmail.com>
2020-05-13 14:44:24 +0530

    May 13, 2020 Problem- Remove K Digits

--- May 12, 2020.py
+++ May 12, 2020.py
@@ -4,6 +4,8 @@
 #Input: [1,1,2,3,3,4,4,8,8], Output: 2
 #Input: [3,3,7,7,10,11,11], Output: 10
 
+#Note: Your solution should run in O(log n) time and O(1) space.
+
 class Solution:
     def singleNonDuplicate(self, nums: List[int])->int:
         left = 0
@@ -21,3 +23,4 @@ class Solution:
                 else:
                     right = mid
         return nums[left]
+
---
+++ May 13, 2020.py
@@ -0,0 +1,20 @@
+#Remove K Digits
+#Given a non-negative integer num represented as a string, remove k digits from the number so that the new number is the smallest possible.
+#Note:
+#The length of num is less than 10002 and will be ≥ k.
+#The given num does not contain any leading zero.
+#Examples:
+#Input: num = "1432219", k = 3, Output: "1219"
+#Explanation: Remove the three digits 4, 3, and 2 to form the new number 1219 which is the smallest.
+#Input: num = "10200", k = 1, Output: "200"
+#Explanation: Remove the leading 1 and the number is 200. Note that the output must not contain leading zeroes.
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+        for digit in num:
+            while k>0 and stack and stack[-1] > digit:
+                stack.pop()
+                k--
+            stack.append(digit)
+        return ''.join(stack[:-k or None]).lstrip("0") or "0"

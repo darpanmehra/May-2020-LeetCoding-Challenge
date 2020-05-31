38a64596354fae4697f92e0d3eee5c6cd587e126
Darpan Mehra <darpan.mehra10@gmail.com>
2020-05-31 21:56:51 +0530

    May 29, 2020 Problem- Course Schedule

---
+++ May 29, 2020.py
@@ -0,0 +1,34 @@
+"""
+207. Course Schedule
+
+There are a total of numCourses courses you have to take, labeled from 0 to numCourses-1.
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+Example 1:
+Input: numCourses = 2, prerequisites = [[1,0]]
+Output: true
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished course 0. So it is possible.
+
+Example 2:
+Input: numCourses = 2, prerequisites = [[1,0],[0,1]]
+Output: false
+Explanation: There are a total of 2 courses to take.
+To take course 1 you should have finished course 0, and to take course 0 you should
+also have finished course 1. So it is impossible.
+"""
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        G = [[] for i in range(numCourses)]
+        degree = [0] * numCourses
+        for j, i in prerequisites:
+            G[i].append(j)
+            degree[j] += 1
+        bfs = [i for i in range(numCourses) if degree[i] == 0]
+        for i in bfs:
+            for j in G[i]:
+                degree[j] -= 1
+                if degree[j] == 0:
+                    bfs.append(j)
+        return len(bfs) == numCourses
